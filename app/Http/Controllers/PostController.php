617af2cd055ae642fbf6fef10ef8ee9b98152090
Davi Lisboa <129395367+davi-ldf@users.noreply.github.com>
2024-07-10 15:35:46 -0300

    Update Post Model and PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,8 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    public function create(Request $request) {
+        $new_post = [
+            'title' => 'Meu Primeiro Post',
+            'content' => 'Conteúdo Qualquer',
+            'author' => 'Davi'
+        ];
+
+        $posts = new Post($new_post);
+
+        dd($posts);
+    }
 }

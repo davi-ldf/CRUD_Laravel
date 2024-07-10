2393cd1ea32d48d7e8495eadbc6969659413d116
Davi Lisboa <129395367+davi-ldf@users.noreply.github.com>
2024-07-10 15:26:19 -0300

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PostController;
+
+Route::get('/posts/create', [PostController::class, 'create']);
+
 Route::get('/', function () {
     return view('welcome');
 });

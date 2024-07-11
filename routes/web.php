6509857b498e5f416bc4ced68e41182671870b17
Davi Lisboa <129395367+davi-ldf@users.noreply.github.com>
2024-07-10 22:36:45 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\PostController;
 
 Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/read', [PostController::class, 'read']);
-Route::get('posts/read_all', [PostController::class], 'all');
+Route::get('/posts/all', [PostController::class, 'all']);
 
 Route::get('/', function () {
     return view('welcome');

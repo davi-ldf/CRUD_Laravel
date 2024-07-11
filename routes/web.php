a7114c97adc129cd069ad6261b9680da73e13d21
Davi Lisboa <129395367+davi-ldf@users.noreply.github.com>
2024-07-10 22:14:17 -0300

    Create read route

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
 Route::get('/posts/create', [PostController::class, 'create']);
+Route::get('/posts/read', [PostController::class, 'read']);
 
 Route::get('/', function () {
     return view('welcome');
